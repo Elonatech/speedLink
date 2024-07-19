682995062b3a99371116ed7224268639190979b6
elonatech <support@elonatech.com.ng>
2024-07-19 17:48:47 +0100

    commit

--- src/components/nav-bar/Nav-bar.js
+++ src/components/nav-bar/Nav-bar.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Nav-bar.css';
 import { IoSearch } from "react-icons/io5";
-import { RiArrowDropDownLine } from "react-icons/ri";
+import { RiArrowDropDownLine, RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import { GoSquare } from "react-icons/go";
 
 const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="logo">
         <img src="https://speedlinkng.com/wp-content/uploads/2023/06/1-223x93.png" alt="Logo" />
       </div>
-      <ul className="nav-links">
+      <div className="menu-icon" onClick={toggleMobileMenu}>
+        {isMobileMenuOpen ? <RiCloseLine size={30} /> : <RiMenu3Line size={30} />}
+      </div>
+      <ul className={`nav-links ${isMobileMenuOpen ? 'nav-links-mobile' : ''}`}>
         <li className="nav-item">
           <a href="#home" className="nav-link">Home</a>
         </li>
@@ -25,36 +35,50 @@ const Navbar = () => {
         </li>
         <li className="nav-item">
           <a href="#services" className="nav-link">Services <RiArrowDropDownLine className='dropdown-icon'/></a>
-          <div className="dropdown">
-            <a href="#itservices" className="dropdown-item">IT Services</a>
-            <a href="#softwaredev" className="dropdown-item">Software Application Development</a>
-            <a href="#webdesign" className="dropdown-item">Web design/development</a>
-            <a href="#softwareinstallations" className="dropdown-item">Software installations</a>
-            <a href="#cloudservices" className="dropdown-item">Cloud Services</a>
-            <a href="#storageinfra" className="dropdown-item">Storage infrastructure</a>
-            <a href="#cloudhosting" className="dropdown-item">Cloud hosting</a>
-            <a href="#cloudbasederp" className="dropdown-item">Cloud based ERP/CRM</a>
-            <a href="#networkingservices" className="dropdown-item">Networking Services</a>
-            <a href="#fibertohome" className="dropdown-item">Fiber-to-the-home</a>
-            <a href="#routing" className="dropdown-item">Routing and Switching</a>
-            <a href="#voip" className="dropdown-item">Voice Over Internet Protocol - VOIP</a>
-            <a href="#isp" className="dropdown-item">Internet Service Provider - ISP</a>
-            <a href="#digitalmarketing" className="dropdown-item">Digital Marketing</a>
-            <a href="#smm" className="dropdown-item">Social Media Marketing</a>
-            <a href="#digitalads" className="dropdown-item">Digital Advertising</a>
-            <a href="#mediaplanning" className="dropdown-item">Media Planning and Buying</a>
-            <a href="#seo" className="dropdown-item">Search Engine Optimization</a>
-            <a href="#techsecurity" className="dropdown-item">Technical Security</a>
-            <a href="#cctv" className="dropdown-item">Video Surveillance (CCTV)</a>
-            <a href="#ips" className="dropdown-item">Intrusion Prevention Systems</a>
-            <a href="#firedetection" className="dropdown-item">Fire detection and fire extinguisher systems</a>
-            <a href="#alarms" className="dropdown-item">Alarm zs</a>
-            <a href="#certtraining" className="dropdown-item">Certified Training</a>
-            <a href="#mikrotik" className="dropdown-item">Mikrotik Certification</a>
-            <a href="#cctvinstall" className="dropdown-item">CCTV Installation</a>
-            <a href="#webappdev" className="dropdown-item">Web app development</a>
-            <a href="#webdesignanddev" className="dropdown-item">Web design and Development</a>
-          </div>
+            <div className="dropdown aboutOne">
+              <div className="grid-container">
+                <div className="grid-item">
+                  <h3>IT Services</h3>
+                  <a href="#softwaredev" className="dropdown-item"><GoSquare className= 'squareIcon'/>Software Application Development</a>
+                  <a href="#webdesign" className="dropdown-item"><GoSquare className= 'squareIcon'/>Web design/development</a>
+                  <a href="#softwareinstallations" className="dropdown-item"><GoSquare className= 'squareIcon'/>Software installations</a>
+                </div>
+                <div className="grid-item">
+                  <h3>Cloud Services</h3>
+                  <a href="#storageinfra" className="dropdown-item"><GoSquare className= 'squareIcon'/>Storage infrastructure</a>
+                  <a href="#cloudhosting" className="dropdown-item"><GoSquare className= 'squareIcon'/>Cloud hosting</a>
+                  <a href="#cloudbasederp" className="dropdown-item"><GoSquare className= 'squareIcon'/>Cloud based ERP/CRM</a>
+                </div>
+                <div className="grid-item">
+                  <h3>Networking Services</h3>
+                  <a href="#fibertohome" className="dropdown-item"><GoSquare className= 'squareIcon'/>Fiber-to-the-home</a>
+                  <a href="#routing" className="dropdown-item"><GoSquare className= 'squareIcon'/>Routing and Switching</a>
+                  <a href="#voip" className="dropdown-item"><GoSquare className= 'squareIcon'/>Voice Over Internet Protocol - VOIP</a>
+                  <a href="#isp" className="dropdown-item"><GoSquare className= 'squareIcon'/>Internet Service Provider - ISP</a>
+                </div>
+                <div className="grid-item">
+                  <h3>Digital Marketing</h3>
+                  <a href="#smm" className="dropdown-item"><GoSquare className= 'squareIcon'/>Social Media Marketing</a>
+                  <a href="#digitalads" className="dropdown-item"><GoSquare className= 'squareIcon'/>Digital Advertising</a>
+                  <a href="#mediaplanning" className="dropdown-item"><GoSquare className= 'squareIcon'/>Media Planning and Buying</a>
+                  <a href="#seo" className="dropdown-item"><GoSquare className= 'squareIcon'/>Search Engine Optimization</a>
+                </div>
+                <div className="grid-item">
+                  <h3>Technical Security</h3>
+                  <a href="#cctv" className="dropdown-item"><GoSquare className= 'squareIcon'/>Video Surveillance (CCTV)</a>
+                  <a href="#ips" className="dropdown-item"><GoSquare className= 'squareIcon'/>Intrusion Prevention Systems</a>
+                  <a href="#firedetection" className="dropdown-item"><GoSquare className= 'squareIcon'/>Fire detection and fire extinguisher systems</a>
+                  <a href="#alarms" className="dropdown-item"><GoSquare className= 'squareIcon'/>Alarm systems</a>
+                </div>
+                <div className="grid-item">
+                  <h3>Certified Training</h3>
+                  <a href="#mikrotik" className="dropdown-item"><GoSquare className= 'squareIcon'/>Mikrotik Certification</a>
+                  <a href="#cctvinstall" className="dropdown-item"><GoSquare className= 'squareIcon'/>CCTV Installation</a>
+                  <a href="#webappdev" className="dropdown-item"><GoSquare className= 'squareIcon'/>Web app development</a>
+                  <a href="#webdesignanddev" className="dropdown-item"><GoSquare className= 'squareIcon'/>Web design and Development</a>
+                </div>
+              </div>
+            </div>
         </li>
         <li className="nav-item">
           <a href="#casestudy" className="nav-link">Case Study <RiArrowDropDownLine className='dropdown-icon'/></a>
